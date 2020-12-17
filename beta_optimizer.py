240d24ad79ee554b5ada1beb7d516bcebfb64162
jonathan-doenz <jonathan.doenz@gmail.com>
2020-12-17 09:00:39 +0100

    Change beta_optimizer to use only O(n) memory

--- beta_optimizer.py
+++ beta_optimizer.py
@@ -6,13 +6,14 @@ Members: Jonathan Doenz, Wentao Feng, Yuxuan Wang
 import numpy as np
 import scipy as sp
 
+from MH import objective
+
 class BetaOptimizerCompetition():
     def __init__(self, dataset, lambda_, n_iterations, verbose=False):
         self.N = len(dataset.v)
         self.g = dataset
         self.lambda_ = lambda_
         self.n_iterations = n_iterations
-        self.distances_matrix = sp.spatial.distance_matrix(self.g.x, self.g.x)
         self.verbose = verbose
 
         # variables recording results
@@ -25,27 +26,16 @@ class BetaOptimizerCompetition():
             f"n_iterations: {self.n_iterations}"
         return s
 
-    def compute_max_distance(self, included_ids):
-        max_distance = self.distances_matrix[np.ix_(included_ids, included_ids)].max()
-        return max_distance
-
-    def compute_f(self, max_distance, population):
-        circular_area = np.pi * (max_distance / 2)**2
-        return self.lambda_ * self.N * circular_area - population
-
     def run(self):
         for i in range(self.n_iterations):
-            # Generate circle with random radius and circle in the [0, 1]^2 square
+            # Generate state composed by only 2 cities
             extreme_ids = np.random.choice(self.N, size=2)
-            max_distance = np.linalg.norm(self.g.x[extreme_ids[0]]
-                                          - self.g.x[extreme_ids[1]])
-            radius = max_distance / 2
-            center = self.g.x[extreme_ids, :].mean(axis=0)
+            state = np.zeros_like(self.g.indices)
+            state[extreme_ids] = 1.
+            state = state.astype(bool)
 
-            # identify nodes inside the circle
-            included_ids = np.where(sp.spatial.distance.cdist(center.reshape(1, -1), self.g.x) < radius)[1]
-            population = self.g.v[included_ids].sum()
-            f_val = self.compute_f(max_distance, population)
+            # Compute objective value for this state
+            f_val = objective(self.g.v, self.g.x, self.lambda_, state)
 
             self.f_vec.append(f_val.copy())
 
